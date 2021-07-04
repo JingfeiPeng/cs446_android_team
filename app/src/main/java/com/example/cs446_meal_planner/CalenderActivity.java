5a9e729d0584b503c03dafbb1d25a3c7308f6e05
Jingfei Peng <jf2peng@edu.uwaterloo.ca>
2021-07-04 02:57:10 -0400

    UI for calender page finished

---
+++ app/src/main/java/com/example/cs446_meal_planner/CalenderActivity.java
@@ -0,0 +1,14 @@
+package com.example.cs446_meal_planner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CalenderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calender);
+    }
+}
