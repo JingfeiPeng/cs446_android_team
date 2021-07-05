65ef2d0bfe4e3505ce8aebba95d676e0648862d5
Jingfei Peng <jf2peng@edu.uwaterloo.ca>
2021-07-05 00:47:16 -0400

    can go forward and backward in week

--- app/src/main/java/com/example/cs446_meal_planner/CalenderActivity.java
+++ app/src/main/java/com/example/cs446_meal_planner/CalenderActivity.java
@@ -3,6 +3,7 @@ package com.example.cs446_meal_planner;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import java.util.Date;
@@ -10,15 +11,26 @@ import java.util.Calendar;
 
 public class CalenderActivity extends AppCompatActivity {
 
+    private int week = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calender);
-        this.updateDates(0);
+        this.updateDates();
     }
 
-    public void updateDates(int week) {
+    public void increaseWeek(View view) {
+        this.week += 1;
+        updateDates();
+    }
+
+    public void decreaseWeek(View view) {
+        this.week -= 1;
+        updateDates();
+    }
+
+    public void updateDates() {
         TextView[] dates = {
                 findViewById(R.id.calender_date1),
                 findViewById(R.id.calender_date2),
@@ -29,11 +41,11 @@ public class CalenderActivity extends AppCompatActivity {
                 findViewById(R.id.calender_date7),
         };
         Date dt = new Date();
-        Calendar c = Calendar.getInstance();
-        c.setTime(dt);
 
         for (int i=0; i < dates.length; i++){
-            c.add(Calendar.DATE, 1+7*week);
+            Calendar c = Calendar.getInstance();
+            c.setTime(dt);
+            c.add(Calendar.DATE, i+7*this.week);
             String[] formatter = c.getTime().toString().split(" ");
             String get_date = "";
             for (int j = 0; j < 3; j++){
