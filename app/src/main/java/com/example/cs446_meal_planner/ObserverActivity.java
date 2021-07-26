be05ba64f33b7400315b7f1e58bde95ad70b9a61
Jingfei Peng <jf2peng@edu.uwaterloo.ca>
2021-07-26 14:07:25 -0400

    Changed recipe list to adapater

---
+++ app/src/main/java/com/example/cs446_meal_planner/ObserverActivity.java
@@ -0,0 +1,7 @@
+package com.example.cs446_meal_planner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+abstract class ObserverActivity extends AppCompatActivity {
+    public void update() {}
+}
